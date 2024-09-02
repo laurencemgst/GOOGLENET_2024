531f80efb2a8090b296d0d134403127d9b29aa32
Laurence Magistrado <89713838+laurencemgst@users.noreply.github.com>
2024-09-02 19:34:05 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("GOOGLENET")
+st.write(
+    "Prototype Visualization."
+)
+
+st.write(
+    """
+    ## Shopee Bicycle Parts and Accessories Reviews
+    DATASET. [here](https://huggingface.co/datasets/lllaurenceee/Shopee_Bicycle_Reviews)
+    """)
